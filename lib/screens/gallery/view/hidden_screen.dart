3d43a681c9ee6973d721100bb4cfe5108693cf11
ashup <ashupal0089@gmail.com>
2024-05-03 13:08:54 +0530

    3-5-24

--- lib/screens/gallery/view/hidden_screen.dart
+++ lib/screens/gallery/view/hidden_screen.dart
@@ -12,7 +12,7 @@ class HiddenScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 80,
+        toolbarHeight: 79,
 
         centerTitle: true,
         // LEADING
